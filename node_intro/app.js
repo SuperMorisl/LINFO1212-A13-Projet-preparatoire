68051768f5b2e15ed3a779e195c4a53b4b80c65d
SuperMorisl <pinnoymaurice2@outlook.com>
2025-10-08 15:10:35 +0200

    finissions du tp 3 express

--- node_intro/app.js
+++ node_intro/app.js
@@ -1,13 +1,21 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+var express = require('express');
+var session = require('express-session');
+var app = express();
+var bodyParser = require("body-parser");
+var fs = require("fs");
+
+app.use(session({
+  secret: 'monsecret',       
+  resave: false,             
+  saveUninitialized: true    
+}));
+
+app.use(express.static(__dirname + '/static'));  // poir les fichiers static
 
 // configurer EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// servir fichiers statiques
-app.use(express.static(path.join(__dirname, 'static')));
+app.set('views', __dirname + '/templates');
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 
 // route de test
@@ -31,20 +39,29 @@ app.get('/hello',(req,res) => {
   `);
 })
 // route page d'accueil
-app.get('/', (req, res) => {
-  res.render('index', { today: new Date() }); 
+app.get('/', function(req, res, next)  {
+  res.render('index', { today: new Date(), username: req.session.username || null }); 
 });
 
 // route login
-app.get('/login', (req, res) => {
-  res.render('login');
+app.get('/login', function(req, res,next)  {
+  res.render('login',{ error: null });
+});
+app.post('/login', function(req,res,next){
+  if (req.body.username && req.body.password == "123pass"){
+  req.session.username = req.body.username;   // stocke dans la session
+  res.redirect('/');  
+  }else {
+    res.render('login', { error: "Mot de passe incorrect" });
+
+  }
+  console.log(req.body);
 });
 
 // route report
-app.get('/report', (req, res) => {
+app.get('/report', function(req, res) {
   res.render('report');
 });
 
-app.listen(8080, () => {
-  console.log("On est good : http://localhost:8080");
-});
+
+app.listen(8080);
